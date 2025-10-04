0c6509b885571f294c17ddabb64dee73c0bd6bb0
podhay123 <kuba.pod123@gmail.com>
2025-10-05 00:13:59 +0200

    fix: correct endpoint

--- app/src/main/java/com/example/batman/ApiService.kt
+++ app/src/main/java/com/example/batman/ApiService.kt
@@ -14,7 +14,7 @@ interface ApiService {
 }
 
 object RetrofitClient {
-    private const val BASE_URL = "http://127.0.0.1:8000/"
+    private const val BASE_URL = "http://10.0.2.2:8000/"
 
     val instance: ApiService by lazy {
         val retrofit = Retrofit.Builder()
